c6fb783f61c0079d8c0ba6bf359aed72ef2a0e42
belrodsal <belrodsal@alum.us.es>
2022-03-25 12:21:28 +0100

    uploaded

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const express = require("express");
+const backend = require("./src/back");
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get("/api",(req,res)=>{
-    res.send({result:true});
-});
-
-
-
-
+backend(app);
 
 app.listen(port, () =>{
     console.log(`server TRULY ready at the port ${port}`);
