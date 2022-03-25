448fbf90e038b649b6347317546e6ed4d20c7ee7
belrodsal <belrodsal@alum.us.es>
2022-03-25 11:03:16 +0100

    updated code

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const cool = require("cool-ascii-faces");
-const { response } = require("express");
+
 const express = require("express");
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
-app.get("/",(req,res)=>{
+app.get("/cool",(req,res)=>{
     console.log("Requested / route");
     res.send("<html><body>"+cool()+"</body></html>")
 });
+
+app.use("/", express.static('public'));
+
+
 
 app.listen(port, () =>{
     console.log(`server TRULY ready at the port ${port}`);
